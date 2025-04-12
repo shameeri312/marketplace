5a07bb7103db966dcc2ae091c19e33e92ba7001a
shameer-entspos <shameer.entspos@gmail.com>
2025-04-13 00:20:09 +0500

    Updated server.ts

--- src/lib/socketClient.ts
+++ src/lib/socketClient.ts
@@ -2,7 +2,7 @@
 
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:4001', {
+export const socket = io('http://localhost:3000', {
   path: '/socket.io',
   transports: ['websocket', 'polling'],
 });
